4c08181bd6174093bd8ce0b4a490667427c34a30
Mohammed Radwan <mohammed-radwan@hotmail.com>
2020-02-17 23:44:27 +0200

    Finalizing the UI and integrate with the API endpoint to render a line chart that displays bitcoin price historical data between two dates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,64 +1,75 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="{{('/css/style.css')}}" rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    Bitcoin Price
-                </div>
-
-                <div style="width: 40%; float: left;"><input type="text" id="from"></div>
-                <div style="width: 40%; float: left;"><input type="text" id="to"></div>
-                <div style="width: 20%; float: left;"><input type="button" value="Render" onclick="render()"></div>
-
-                <div>
-                    <canvas id="bitcoin-prices-chart"></canvas >
-                </div>
-            </div>
+    <!-- Styles -->
+    <link href="{{('/css/style.css')}}" rel='stylesheet' type='text/css'>
+    </link>
+</head>
+<body>
+<div class="flex-center  full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Bitcoin Price
         </div>
 
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div class="text-danger" id="errors"></div>
 
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+        <div style="width: 40%; float: left;"><input type="text" id="from"></div>
+        <div style="width: 40%; float: left;"><input type="text" id="to"></div>
+        <div style="width: 20%; float: left;"><input type="button" value="Render" onclick="render()"></div>
 
-        <script>
-            $('#from').datepicker();
-            $('#to').datepicker();
+        <div>
+            <canvas id="bitcoin-prices-chart"></canvas >
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-            function render() {
-                var ctx = document.getElementById('bitcoin-prices-chart').getContext('2d');
-                var chart = new Chart(ctx, {
-                    // The type of chart we want to create
-                    type: 'line',
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
-                    // The data for our dataset
-                    data: {
-                        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                        datasets: [{
-                            label: 'My First dataset',
-                            backgroundColor: 'rgb(255, 99, 132)',
-                            borderColor: 'rgb(255, 99, 132)',
-                            data: [0, 10, 5, 2, 20, 30, 45]
-                        }]
-                    },
+<script>
+    $('#from').datepicker({
+        dateFormat: "yy-mm-dd"
+    }).datepicker(
+        'setDate', '-7d'
+    );
 
-                    // Configuration options go here
-                    options: {}
-                });
+    $('#to').datepicker({
+        setDate: '-1d',
+        defaultDate: '-1d',
+        dateFormat: "yy-mm-dd"
+    }).datepicker(
+        'setDate', '0d'
+    );
+
+    function render() {
+        let url = "/api/chartjs/line?from=" + $('#from').val() + "&to=" + $('#to').val();
+
+        $('#errors').html('');
+
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            async: false,
+            success: function(data) {
+                let el = document.getElementById('bitcoin-prices-chart');
+                let chart = new Chart(el, data);
+            },
+            error: function (error) {
+                //TODO: format errors markup
+                $('#errors').html(JSON.stringify(error.responseJSON.errors));
             }
-        </script>
-    </body>
+        });
+    }
+</script>
+</body>
 </html>
